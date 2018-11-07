c4c945ea85a8f61cb5920b8c386f74bcde8fdc0d
Sudhanshu Vohra <sudhanshuvohra8443@gmail.com>
2018-11-07 06:27:08 +0530

    Replaced the Botton Navigation Bar with a Bottom Sheet Layout containing three buttons, added back button in News Details Display activity, implemented share and view in browser features in New Details Display activity

--- app/src/main/java/developersudhanshu/com/newsdash/activities/NewsDetailDisplayActivity.java
+++ app/src/main/java/developersudhanshu/com/newsdash/activities/NewsDetailDisplayActivity.java
@@ -1,7 +1,14 @@
 package developersudhanshu.com.newsdash.activities;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -11,11 +18,14 @@ import developersudhanshu.com.newsdash.R;
 import developersudhanshu.com.newsdash.models.NewsFeedModel;
 import developersudhanshu.com.newsdash.utility.Constants;
 
-public class NewsDetailDisplayActivity extends AppCompatActivity {
+public class NewsDetailDisplayActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView newsHeadline, newsDate, newsAuthor;
     private ImageView newsImage;
     private TextView newsDescription;
+    private Button shareStory, viewFullStory, markFavoriteStory;
+    private String newsUrl;
+    private static final String TAG = NewsDetailDisplayActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +38,23 @@ public class NewsDetailDisplayActivity extends AppCompatActivity {
             NewsFeedModel model = getIntent().getParcelableExtra(Constants.NEWS_FEED_INTENT_EXTRA_KEY);
             populateViewsWithData(model);
         }
+
+        if (getSupportActionBar() != null) {
+            // Enable back button
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+            // Disable title
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void populateViewsWithData(NewsFeedModel model) {
@@ -36,6 +63,9 @@ public class NewsDetailDisplayActivity extends AppCompatActivity {
         newsDate.setText(model.getDate());
         newsAuthor.setText(model.getAuthorName());
         newsDescription.setText(model.getDescription());
+
+        // Saving the news URL to be shared and opened in Browser
+        newsUrl = model.getNewsUrl();
 
         // Loading the image in the image view
         Picasso.with(this)
@@ -51,5 +81,42 @@ public class NewsDetailDisplayActivity extends AppCompatActivity {
         newsAuthor = findViewById(R.id.tv_author_act_news_detail_display);
         newsDescription = findViewById(R.id.tv_desc_act_news_detail_display);
         newsImage = findViewById(R.id.img_view_act_news_detail);
+
+        markFavoriteStory = findViewById(R.id.btn_mark_fav_act_news_details_display);
+        shareStory = findViewById(R.id.btn_share_act_news_details_display);
+        viewFullStory = findViewById(R.id.btn_open_in_browser_act_news_details_display);
+
+        markFavoriteStory.setOnClickListener(this);
+        shareStory.setOnClickListener(this);
+        viewFullStory.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_mark_fav_act_news_details_display:
+                break;
+            case R.id.btn_share_act_news_details_display:
+                if (!TextUtils.isEmpty(newsUrl)) {
+                    Intent shareStoryIntent = new Intent();
+                    shareStoryIntent.setType("text/plain");
+                    shareStoryIntent.setAction(Intent.ACTION_SEND);
+                    shareStoryIntent.putExtra(Intent.EXTRA_TEXT, newsUrl);
+                    startActivity(shareStoryIntent);
+                } else {
+                    Log.d(TAG, "News Url is Empty");
+                }
+                break;
+            case R.id.btn_open_in_browser_act_news_details_display:
+                if (!TextUtils.isEmpty(newsUrl)) {
+                    Intent openInBrowserIntent = new Intent();
+                    openInBrowserIntent.setAction(Intent.ACTION_VIEW);
+                    openInBrowserIntent.setData(Uri.parse(newsUrl));
+                    startActivity(Intent.createChooser(openInBrowserIntent, "Open with"));
+                } else {
+                    Log.d(TAG, "News Url is Empty");
+                }
+                break;
+        }
     }
 }
